9bbe1c4467659a76d571f5b298370236f0baf178
shreyk98 <shreyk98@g.ucla.edu>
2018-07-15 14:06:04 -0700

    Login done

--- gyde/gyde1/views.py
+++ gyde/gyde1/views.py
@@ -13,30 +13,36 @@ def home(request):
     return render(request, 'homepage/homepage.html', )
 #   will use render instead
 
-
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.level = form.cleaned_data.get('level')
+            # user.profile.level = form.cleaned_data.get('level')
             user.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('profile')
     else:
         form = SignUpForm()
     return render(request, 'loginpage/signup.html', {'form': form})
 
 
-def login(request):
-        return render(request, 'loginpage/login.html', )
+# def login(request):
+#         return render(request, 'loginpage/login.html', )
+
 
 
 def profile(request):
-        return render(request, 'profilepage/user_profile.html', )
+    username1 = None
+    if request.user.is_authenticated:
+        username1 = request.user.username
+        user = User.objects.get(username=username1)
+    else:
+        user = User.objects.get(username='Gyde_mert')
+    return render(request, 'profilepage/user_profile.html', {'users': user})
 
 
 def newsfeed(request):
